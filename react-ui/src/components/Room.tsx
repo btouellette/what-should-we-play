80420bc8b7cf23e875af9d6ecf0ad4b38c7cffa5
Brian Ouellette <btouellette@gmail.com>
2020-12-13 13:45:13 -0800

    basic layout for room component started

--- react-ui/src/components/Room.tsx
+++ react-ui/src/components/Room.tsx
@@ -1,13 +1,26 @@
-import {  useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { toJSON, logError } from "../helpers/promiseHelpers";
+import { IRoom } from "../../../server/models/room"
 
 const Room = () => {
+  const [room, setRoom] = useState({});
+
+  useEffect(() => {
+    fetch('/api/get-room', { method: 'GET' })
+    .then(toJSON)
+    .then((data: IRoom) => {  })
+    .catch(logError)
+    .finally(() => {  });
+  });
   // We can use the `useParams` hook here to access
   // the dynamic pieces of the URL.
-  let { room } = useParams<Record<string, string>>();
+  let { roomName } = useParams<Record<string, string>>();
 
   return (
     <div>
-      <h3>ID: {room}</h3>
+      <h3>ID: {roomName}</h3>
     </div>
   );
 }
