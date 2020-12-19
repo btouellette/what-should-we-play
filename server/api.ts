2fc65010e250dc11c98b6fed823a1a2da5588e7d
Brian Ouellette <btouellette@gmail.com>
2020-12-18 19:45:07 -0800

    switch to async mongoose api

--- server/api.ts
+++ server/api.ts
@@ -6,59 +6,70 @@ import Room from './models/room';
 export const createRoom = (req: Request, res: Response) => {
   log.debug('Creating new room');
   // How many retries on name generation before giving up (currently 1500*1500*1728 possible room names)
-  let safetyValve = 10;
+  let safetyValve = 3;
 
-  (function createRoom() {
+  (async function createRoom() {
     // Generate new named room and attempt to save to data store
     const room = new Room({
       name: generateName()
     });
-    room.save((err: any) => {
-      if (err) {
-        if (err.name === 'MongoError' && err.code === 11000) {
-          // Room name already in use, retry name generation up to limit
-          if (safetyValve-- <= 0) {
-            log.error('Failed to create room due to safety valve');
-            res.set('Content-Type', 'text/plain');
-            res.status(500).send('Could not find unique room name');
-          } else {
-            createRoom()
-          }
-        } else {
-          // Unknown error occurred
-          log.error(`Failed to create room: ${err}`);
+    try {
+      await room.save();
+      // Successfully created room, return it to front end
+      log.debug(`Created room: ${room.name}`);
+      res.set('Content-Type', 'application/json');
+      res.send(room.name);
+    } catch (err) {
+      if (err.name === 'MongoError' && err.code === 11000) {
+        // Room name already in use, retry name generation up to limit
+        if (safetyValve-- <= 0) {
+          log.error('Failed to create room due to safety valve');
           res.set('Content-Type', 'text/plain');
-          res.status(500).send('Failed to create new room');
+          res.status(500).send('Could not find unique room name');
+        } else {
+          createRoom()
         }
       } else {
-        // Successfully created room, return it to front end
-        log.debug(`Created room: ${room.name}`);
-        res.set('Content-Type', 'application/json');
-        res.send(room.name);
+        // Unknown error occurred
+        log.error(`Failed to create room: ${err}`);
+        res.set('Content-Type', 'text/plain');
+        res.status(500).send('Failed to create new room');
       }
-    });
+    }
   })();
 };
 
-export const getRoom = (req: Request, res: Response) => {
-  Room.findOne({ name: req.query.name as string })
-  .then((room) => {
-    if (room) {
-      // Found room in data store, return it to front end
-      res.set('Content-Type', 'application/json');
-      res.send(room);
-    } else {
-      // Unknown error occurred fetching room, possibly one does not exist with this name
-      log.error(`Failed to find room: ${req.query.name}`);
-      res.set('Content-Type', 'text/plain');
-      res.status(500).send('Failed to find room');
-    }
-  });
+export const getRoom = async (req: Request, res: Response) => {
+  try {
+    let roomData = await Room.findOne({ name: req.query.name as string });
+    // Found room in data store, return it to front end
+    res.set('Content-Type', 'application/json');
+    res.send(roomData);
+  } catch (err) {
+    // Error occurred fetching room, possibly one does not exist with this name
+    log.error(`Failed to find room ${req.query.name}: ${err}`);
+    res.set('Content-Type', 'text/plain');
+    res.status(500).send('Failed to find room');
+  }
 };
 
-export const addOption = (req: Request, res: Response) => {
-  Room.findOneAndUpdate({ name: req.query.name as string })
-  .then((room) => {
-
-  });
+export const addOption = async (req: Request, res: Response) => {
+  try {
+    const filter = {
+      name: req.query.roomName as string,
+      'options.name': { $ne: req.query.optionName as string }
+    };
+    const update = {
+      $push: {
+        options: {
+          name: req.query.optionName as string,
+          userVotes: [req.query.userName as string]
+        }
+      }
+    };
+    let roomData = await Room.findOneAndUpdate(filter, update, { new: true });
+  } catch (err) {
+    log.error(`Failed to add option: ${err}`);
+    res.status(500).send('Failed to add option');
+  }
 };
